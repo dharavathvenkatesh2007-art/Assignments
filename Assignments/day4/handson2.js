5c9e2ae34d8118d936577323ff33b84591039aeb
dharavathvenkatesh2007-art <dharavathvenkatesh2007@gmail.com>
2026-02-27 23:06:28 +0530

    handson2.js

--- Assignments/day4/handson2.js
+++ Assignments/day4/handson2.js
@@ -1,7 +1,7 @@
 /*Hands-On 2: Deep Copy (Isolation & Safety Use Case)
 ---------------------------------------------------
 
-🧪 Given Data:*/
+ Given Data:*/
                 const order = {
                   orderId: "ORD1001",
                   customer: {
@@ -16,7 +16,7 @@
                   ]
                 };
 /*
-🎯 Task:
+Task:
       1. Create a deep copy of order
       2. Modify in copied object:
             i. customer.address.city
